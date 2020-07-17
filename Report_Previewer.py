e98dba62ebbe52dc2a09ba663f6e314e8b7ee153
Mark Fawcett <markjfawcett@gmail.com>
2020-07-17 12:50:17 +0100

    Add waiting for files message after runs

--- Report_Previewer.py
+++ Report_Previewer.py
@@ -47,7 +47,7 @@ def main():
     event_handler = PatternMatchingEventHandler(patterns, ignore_patterns,
                                                 ignore_directories=True,
                                                 case_sensitive=True)
-    event_handler.on_created = on_created
+    event_handler.on_created = lambda x: on_created(x, watched_Path)
 
     # create cross platform path to folder to watch
     watched_Path = Path(Path.home(), 'committee_report_html/word_filtered_html')
@@ -60,7 +60,8 @@ def main():
     my_observer.schedule(event_handler, str(watched_Path),
                          recursive=False)  # not interested in sub-directories
 
-    print('\nWaiting for files to be added to:\n  {}\n'.format(watched_Path))
+    # print('\nWaiting for files to be added to:\n  {}\n'.format(watched_Path))
+    wait_msg(watched_Path)
     my_observer.start()
     try:
         while True:
@@ -70,6 +71,8 @@ def main():
         my_observer.join()
 
 
+def wait_msg(watched_Path):
+    print('\nWaiting for files to be added to:\n  {}\n'.format(watched_Path))
 
 def roubust_execution(funcs, input_html):
     for func in funcs:
@@ -89,7 +92,7 @@ def roubust_execution(funcs, input_html):
                 feedback.writeln('\t\t' + str(e))
 
 
-def on_created(event):
+def on_created(event, watched_Path):
 
     def wait_for_file(file):
         """ wait for file to finish being created or copied"""
@@ -134,16 +137,16 @@ def on_created(event):
         else:
             input_params[key_value[0].strip()] = key_value[1].strip()
 
-    # create ReportHTML obj
-    # output_html.set_up(html_Path)
 
     # input_html = ReportHTML(html_Path, parameters)
-
     process_html(html_Path, input_params)
 
     # I wonder if we should now delete both parameters.txt and the html_Path
     html_Path.unlink(missing_ok=True)  # deletes the file
     parameters_file.unlink(missing_ok=True)
+
+    # reprint wait message
+    wait_msg(watched_Path)
 
 
 def check_metadate(metadata: Dict[str, str]) -> Dict[str, str]:
@@ -154,8 +157,7 @@ def check_metadate(metadata: Dict[str, str]) -> Dict[str, str]:
     for key, value in expected_parameters.items():
         if not value:
             feedback.warning(f'Expected parameter {key} is either not present or has no value.'
-                              ' The outputted HTML will be incomplete without this.'
-                              ' Do not publish the HTML until this is fixed.')
+                              ' The outputted HTML will be incomplete without this.')
 
     return expected_parameters
 
@@ -163,7 +165,7 @@ def check_metadate(metadata: Dict[str, str]) -> Dict[str, str]:
 # def process_html(input_html: ReportHTML, input_Path: Path):
 def process_html(input_html_Path: Path, metadata: Dict[str, str]):
 
-    print('\nProcessing HTML...\n')
+    print('\nProcessing HTML....\n')
 
 
     # process the metadata
@@ -175,7 +177,9 @@ def process_html(input_html_Path: Path, metadata: Dict[str, str]):
     metadata['committee_address'], metadata['committee_publications'] = committee_address
 
 
-    output_html.set_up(input_html_Path)
+    # if the report title has been passed in as we should add it to the output
+    # output_html.set_up(input_html_Path, report_title=metadata['report_title'])
+    ReportHTML.set_up(input_html_Path, metadata)
 
     input_html = ReportHTML(input_html_Path, metadata)
 
@@ -227,35 +231,52 @@ def process_html(input_html_Path: Path, metadata: Dict[str, str]):
              univ_html.replace_back_pages)
     roubust_execution(funcs, input_html.root)
 
+    # fix images
+    fix_images(input_html.root)
+
     summary, report = output_html.separate_summary(input_html)
 
     if summary:  # could be None
         print('\nCreating summary:')
-        summary.write(output_html.OutputPaths['summary_web'])
+        # summary.write(output_html.OutputPaths['summary_web'])
+        summary.write(ReportHTML.summary_web)
 
         # lets also have a go at creating a print-summary
         print_summary = summary.make_print_version()
         if print_summary:
-            print_summary.write(output_html.OutputPaths['summary_print'], open_in_browser=False)
+            # print_summary.write(output_html.OutputPaths['summary_print'], open_in_browser=False)
+            print_summary.write(ReportHTML.summary_print, open_in_browser=False)
     else:
         feedback.warning('Summary not created')
 
     if report:
         print('\nCreating full report')
-        report.write(output_html.OutputPaths['report_web'])
+        # report.write(output_html.OutputPaths['report_web'])
+        report.write(ReportHTML.report_web)
 
         print_report = report.make_print_version()
         if print_report:
-            print_report.write(output_html.OutputPaths['report_print'], open_in_browser=False)
+            # print_report.write(output_html.OutputPaths['report_print'], open_in_browser=False)
+            print_report.write(ReportHTML.report_print, open_in_browser=False)
     else:
         feedback.warning('Full report not created')
 
     feedback.writeln('\n  ' + 'All Done!\n')
 
 
+def fix_images(root):
+    # we need to move the
+    word_folder_name = 'word_filtered_html_files'
+    images = root.xpath(f'//*[@*[contains(., "{word_folder_name}")]]')
+    for images in images:
+        for key, value in images.attrib.items():
+            images.attrib[key] = value.replace(word_folder_name, ReportHTML.img_folder)
+
+
 if __name__ == "__main__":
     try:
-        os.system('color')  # coloured text in windows cmd prompt
+        if os.name == 'nt':
+            os.system('color')  # coloured text in windows cmd prompt
         main()
     except Exception:
         print("Exception in user code:\n")
