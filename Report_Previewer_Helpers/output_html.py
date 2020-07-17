4f0249303f166f99a12c44f4c5abd152c0159a69
Mark Fawcett <markjfawcett@gmail.com>
2020-07-17 12:48:21 +0100

    Fix issue report not output when there is no summary heading

--- Report_Previewer_Helpers/output_html.py
+++ Report_Previewer_Helpers/output_html.py
@@ -2,9 +2,11 @@ from __future__ import annotations  # for type annotations
 from copy import deepcopy
 import os
 from pathlib import Path
-from typing import Optional, Tuple, Union
+import re
+from typing import Dict, Optional, Tuple, Union
 import webbrowser
 from distutils import dir_util
+from distutils.errors import DistutilsFileError
 
 # Import third-party modules
 from lxml import html  # type: ignore
@@ -16,38 +18,116 @@ import Report_Previewer_Helpers.settings as st
 from .settings import REQUIRED as Required
 
 
-OutputPaths = {'report_web': Path(st.REPORT),
-               'summary_web': Path(st.SUMMARY),
-               'report_print': Path(st.REPORT_PRINT),
-               'summary_print': Path(st.SUMMARY_PRINT)}
+# # must be copied each time we run
+# OutputPaths = {}
 
-def set_up(input_htmp_Path: Path):
+# def set_up(input_htmp_Path: Path, report_title: str = ''):
 
-    output_folder_Path = input_htmp_Path.parent.parent
+#     # copy all the output paths - must happen every time we run
+#     global OutputPaths
+#     OutputPaths = {'report_web': Path(st.REPORT),
+#                    'summary_web': Path(st.SUMMARY),
+#                    'report_print': Path(st.REPORT_PRINT),
+#                    'summary_print': Path(st.SUMMARY_PRINT)}
 
-    for key, value in OutputPaths.items():
-        OutputPaths[key] = output_folder_Path / value
+#     # we don't want any stupidly long file names so lets tuncate them
+#     report_title = report_title[:50]
+#     # ascii only
+#     report_title = report_title.encode('ascii', 'ignore').decode()
+#     # make lowercase and remove space from ends
+#     report_title = report_title.strip().lower()
+#     # dont allow anything that is not a word, space or -
+#     report_title = re.sub(r'[^\w\s-]', '', report_title)
+#     # no spaces
+#     report_title = re.sub(r'[-\s]+', '-', report_title)
 
-    # copy css etc
-    # try:
-    Required['templates_dir']
-    dir_util.copy_tree(Required['templates_dir'], str(output_folder_Path))
-    # except:
-    #     pass
+#     output_folder_Path = input_htmp_Path.parent.parent
+
+#     for key, value in OutputPaths.items():
+#         file_name = f'{report_title}-{value}'
+#         OutputPaths[key] = output_folder_Path / file_name
+
+#     # copy css etc
+#     # try:
+#     Required['templates_dir']
+#     dir_util.copy_tree(Required['templates_dir'], str(output_folder_Path))
+#     # except:
+#     #     pass
 
 
 class ReportHTML:
 
-    # print(Required.get('templates_dir'))
-    def __init__(self, html_: Union[Path, _Element], meta: dict):
+    # temporary values
+    report_web = Path(st.REPORT)
+    summary_web = Path(st.SUMMARY)
+    report_print = Path(st.REPORT_PRINT)
+    summary_print = Path(st.SUMMARY_PRINT)
+
+    img_folder = ''
+
+    # _set_up_done = False
+
+    @classmethod
+    def set_up(cls, html_path: Union[Path, str], meta: dict):
+
+        print('Copying files.')
+
+        html_Path = Path(html_path)
+
+        file_title = meta.get('report_title', '')
+        # we don't want any stupidly long file names so lets truncate them
+        file_title = file_title[:50]
+        # ascii only
+        file_title = file_title.encode('ascii', 'ignore').decode()
+        # make lowercase and remove space from ends
+        file_title = file_title.strip().lower()
+        # don't allow anything that is not a word, space or -
+        file_title = re.sub(r'[^\w\s-]', '', file_title)
+        # no spaces
+        file_title = re.sub(r'[-\s]+', '-', file_title)
+
+        cls.img_folder = f'{file_title}-files'
+
+        output_folder_Path = html_Path.parent.parent
+
+        # affect the class attributes
+        ReportHTML.report_web    = output_folder_Path / f'{file_title}-{st.REPORT}'
+        ReportHTML.summary_web   = output_folder_Path / f'{file_title}-{st.SUMMARY}'
+        ReportHTML.report_print  = output_folder_Path / f'{file_title}-{st.REPORT_PRINT}'
+        ReportHTML.summary_print = output_folder_Path / f'{file_title}-{st.SUMMARY_PRINT}'
+
+        # print(ReportHTML.report_web, ReportHTML.summary_web,
+        #       ReportHTML.report_print, ReportHTML.summary_print, sep='\n')
+
+        # copy css etc
+        try:
+            Required['templates_dir']
+            dir_util.copy_tree(Required['templates_dir'], str(output_folder_Path))
+        except:
+            feedback.error('Could not copy CSS etc. HTML file may not appear styled correctly.')
+
+        # copy any images that may be in a word_filtered_html_files folder
+        # remember to also change all the paths in the html
+        try:
+            dir_util.copy_tree(str(html_Path.parent / 'word_filtered_html_files'),
+                               str(output_folder_Path / cls.img_folder))
+        except DistutilsFileError:
+            pass
+        except:
+            feedback.error('Could not copy any Images etc.')
+
+        # cls._set_up_done = True
+
+    def __init__(self, html_path: Union[Path, str], meta: dict):
         self.metadata = meta
 
-        if isinstance(html_, _Element):
-            self.root = html_
-            self.html_tree = ElementTree(self.root)
-        else:
-            self.html_tree = html.parse(str(html_))
-            self.root = self.html_tree.getroot()
+        # lxml requires a string (not Path)
+        self.html_tree = html.parse(str(html_path))
+        self.root = self.html_tree.getroot()
+
+        # if not self._set_up_done:
+        #     ReportHTML._set_up(html_path, meta)
+
 
 
     def write(self, output_file_path: Union[Path, str], open_in_browser=True, verbose=True):
@@ -65,7 +145,7 @@ class ReportHTML:
         try:
             if open_in_browser:
                 if os.name == 'posix':
-                    webbrowser.open('file://' + output_file_path)
+                    webbrowser.open('file://' + str(Path(output_file_path).resolve()))
                 else:
                     webbrowser.open(output_file_path)
         except:
@@ -161,6 +241,13 @@ def separate_summary(html_: ReportHTML) -> Tuple[Optional[OutputHTML], Optional[
                     report_start = sibling
                     break
 
+    # sometimes there is no summary
+    if not report_start:
+        xpath = '//h2 |  //*[@class="SummaryHeading"] | //*[@class="ChapterHeading1"]'
+        headings = html_.root.xpath(xpath)
+        if headings:
+            report_start = headings[0]
+
     summary_e = html.Element('div', attrib={'id': 'summary'})
     summary_e.tail = '\n'
 
@@ -175,7 +262,7 @@ def separate_summary(html_: ReportHTML) -> Tuple[Optional[OutputHTML], Optional[
         move_sibs(summary_start, report_start, summary_e)
         summary = _web_html(summary_e, 'summary', html_.metadata)
 
-    if summary_start is not None:
+    if report_start is not None:
         move_sibs(report_start, None, report_e)
         report = _web_html(report_e, 'report', html_.metadata)
 
@@ -186,12 +273,14 @@ def separate_summary(html_: ReportHTML) -> Tuple[Optional[OutputHTML], Optional[
         summary.root.find(p_epath).text  = 'This is the report summary, '
         pa_ele = summary.root.find(pa_epath)
         pa_ele.text = 'read the full report'
-        pa_ele.set('href', str(OutputPaths['report_web'].name))
+        # pa_ele.set('href', str(OutputPaths['report_web'].name))
+        pa_ele.set('href', str(ReportHTML.report_web.name))
     if report:
         report.root.find(p_epath).text   = 'This is the full report, '
         pa_ele = report.root.find(pa_epath)
         pa_ele.text = 'read the report summary'
-        pa_ele.set('href', str(OutputPaths['summary_web'].name))
+        # pa_ele.set('href', str(OutputPaths['summary_web'].name))
+        pa_ele.set('href', str(ReportHTML.summary_web.name))
 
     return summary, report
 
